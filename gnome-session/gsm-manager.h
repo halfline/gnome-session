a466a01de41e86e80bb5276366126a4938aa60bb
Josselin Mouette <joss@malsain.org>
2010-06-21 15:22:23 -0400

    Add "Remember Currently Running Applications" button
    This adds back session saving that's not at logout.

--- gnome-session/gsm-manager.h
+++ gnome-session/gsm-manager.h
@@ -161,6 +161,9 @@ gboolean            gsm_manager_shutdown                       (GsmManager     *
 gboolean            gsm_manager_reboot                         (GsmManager     *manager,
                                                                 GError        **error);
 
+gboolean            gsm_manager_save_session                   (GsmManager     *manager,
+                                                                GError        **error);
+
 gboolean            gsm_manager_can_shutdown                   (GsmManager     *manager,
                                                                 gboolean       *shutdown_available,
                                                                 GError        **error);
